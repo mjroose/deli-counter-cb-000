563d663ff4a16cedbe6e4739f80f9b27414d6a2d
Michael Roose <mjroose@gmail.com>
2018-08-05 17:23:49 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,17 @@
-# Write your code here.
+katz_deli = []
+
+def line(customers)
+  message = "The line is currently"
+  if customers.length == 0
+    message += " empty."
+    puts "The line is currently empty."
+  else
+    message += ":"
+    customers.each_with_index do |customer, index|
+      message += " #{index + 1}. #{customer}"
+    end
+  end
+  puts message
+end
+
+  
